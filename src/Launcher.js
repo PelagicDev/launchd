71946856c0cfb954f10c985353330c4c5b345a9d
Jordan Godwin <pelagic.dev@gmail.com>
2020-04-30 15:16:37 -0400

    Add react-hook-form w/ simple launch form;

--- src/Launcher.js
+++ src/Launcher.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+
 import {
   Box,
   Button,
@@ -10,27 +12,75 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
+  Select,
   useDisclosure,
 } from '@chakra-ui/core'
 
+const BOATS = [
+  { id: 1, name: "23' Clearwater" },
+  { id: 2, name: "28' Regulator" },
+  { id: 3, name: "16' Boston Whaler" },
+  { id: 4, name: "22' Sea Hunt" },
+  { id: 5, name: "35' Triton" },
+]
+
 const Launcher = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const { register, handleSubmit, errors } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+    onClose()
+  }
+
+  const NewLaunchForm = () => {
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ModalBody>
+          {/* register your input into the hook by invoking the "register" function */}
+          <Select
+            name="boat"
+            placeholder="Select boat"
+            ref={register({ required: true })}
+            mb={2}
+          >
+            {BOATS.map((b) => (
+              <option key={b.id} value={b.id}>
+                {b.name}
+              </option>
+            ))}
+          </Select>
+          <Select name="placement" defaultValue="dock" ref={register} mb={2}>
+            <option value="dock">Dock</option>
+            <option value="fishing-am">Fishing (early morning)</option>
+            <option value="fuel">Fuel (Wet Slip)</option>
+            <option value="after-hours">Request After Hours</option>
+          </Select>
+
+          {errors.boat && <span>This field is required</span>}
+        </ModalBody>
+
+        <ModalFooter>
+          <Button variant="ghost" mr={3} onClick={onClose}>
+            Close
+          </Button>
+          <Button variantColor="blue" type="submit">
+            Save Launch
+          </Button>
+        </ModalFooter>
+      </form>
+    )
+  }
 
   return (
     <Flex justifyContent="center" className="launcher">
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay>
           <ModalContent>
-            <ModalHeader>Modal Title</ModalHeader>
+            <ModalHeader>New Launch</ModalHeader>
             <ModalCloseButton />
-            <ModalBody>LAUNCH FORM</ModalBody>
-
-            <ModalFooter>
-              <Button variantColor="blue" mr={3} onClick={onClose}>
-                Close
-              </Button>
-              <Button variant="ghost">Secondary Action</Button>
-            </ModalFooter>
+            <NewLaunchForm />
           </ModalContent>
         </ModalOverlay>
       </Modal>
